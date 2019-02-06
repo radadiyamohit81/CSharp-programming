87c8935d2a0149be7c556bf2b850ae80aa7380b9
radadiyamohit81 <30775542+radadiyamohit81@users.noreply.github.com>
2019-02-06 05:56:36 +0530

    Write a program to get message from user using command line argument.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace CommandLineArgument
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (String arg in Environment.GetCommandLineArgs())
+            {
+                Console.Write(arg);
+            }
+        }
+    }
+}
